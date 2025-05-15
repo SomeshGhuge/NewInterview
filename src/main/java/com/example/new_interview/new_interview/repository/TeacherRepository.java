c2e5f0579832e4ec6454fecb467beb45cb8effb5
SomeshGhuge <someshghuge85@gmail.com>
2025-05-15 23:45:25 +0530

    added code required

--- src/main/java/com/example/new_interview/new_interview/repository/TeacherRepository.java
+++ src/main/java/com/example/new_interview/new_interview/repository/TeacherRepository.java
@@ -1,1 +1,9 @@
- 
+package com.example.new_interview.new_interview.repository;
+
+import com.example.new_interview.new_interview.model.Teacher;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+}
